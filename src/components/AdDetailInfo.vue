b6e58913ecc8ef4c359885e483d12d9ade5b21fe
ahmed adam <ahmedadammohammed.abduallah@gmail.com>
2025-11-14 20:33:40 +0200

    fix add details locale error

--- src/components/AdDetailInfo.vue
+++ src/components/AdDetailInfo.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="ad-detail flex flex-col justify-center p-2">
     <div class="flex justify-between items-center">
-      <p>{{ i18?.locale.value == "ar" ? detail.label : detail.label_en }}</p>
-      <p>{{ i18?.locale.value == "ar" ? detail.value : detail.value_en }}</p>
+      <p>{{ currentLocale == "ar" ? detail?.label : detail?.label_en }}</p>
+      <p>{{ currentLocale == "ar" ? detail?.value : detail?.value_en }}</p>
     </div>
 
     <hr class="my-2 border-gray-300" />
@@ -10,10 +10,13 @@
 </template>
 
 <script setup>
+import { computed } from "vue";
 import { useI18n } from "vue-i18n";
 defineProps(["detail"]);
 
-const i18 = useI18n();
+const { locale } = useI18n();
+
+const currentLocale = computed(() => locale.value || "ar");
 </script>
 
 <style scoped>
